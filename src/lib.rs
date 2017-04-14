1d0d36eda723e065948d52ab7201b12dc4361b3b
Hunar Roop Kahlon <hunar.roop@gmail.com>
2017-04-14 11:39:50 -0700

    add base doc info
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+/*
+ Crate:         nbf
+ File:          /lib.rs
+ Module:        ::
+ Visibility:    Public
+ */
+
+//! Node Based Format is a Serialization and Deserialization library.
+//!
+//! # Usage
+//!
+//! Add this to your `Cargo.toml`:
+//!
+//! ```toml
+//! [dependencies]
+//! nbf = "0.1.0"
+//! ```
+//!
+//! and this to your crate root:
+//!
+//! ```rust
+//! extern crate nbf;
+//! ```
